21c7b08c241cdbffeaba3d63ad2eade02737907e
David Fox <davidmfox123@gmail.com>
2025-04-25 11:46:21 +0100

    Update submitApp.js

--- api/submitApp.js
+++ api/submitApp.js
@@ -31,7 +31,7 @@ const sendNewAppEmail = async (app) => {
     if (process.env.RESEND_API_KEY) {
       const resend = new Resend(process.env.RESEND_API_KEY);
       await resend.emails.send({
-        from: 'ZAPT Affiliate Marketplace <notifications@resend.dev>',
+        from: 'ZAPT Affiliate Marketplace <noreply@zapt.ai>',
         to: 'david@zapt.ai',
         subject: 'New Affiliate App Submission',
         html: `
